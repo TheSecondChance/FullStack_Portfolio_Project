d65f340cc28253c4fe50dd855a1b8255ad16dea6
TheSecondChance <mesakmegerem@gmail.com>
2024-11-21 02:11:37 +0300

    Add Pages component

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,15 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import Pages from "./Pages/Pages";
 
 const App = () => {
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route path="/" element={<MainLayout />}></Route>)
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Pages />} />
+      </Route>
+    )
   );
   return <RouterProvider router={router} />;
 };
